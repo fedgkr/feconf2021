7747ed406c7d24be11ed1bd9a95cc5dc5d00e788
codemilli <hckrmoon@gmail.com>
2021-10-05 10:57:02 +0900

    Lazy load 제거

--- src/components/SpeakerSection/components/SpeakerCard/SpeakerCard.tsx
+++ src/components/SpeakerSection/components/SpeakerCard/SpeakerCard.tsx
@@ -1,21 +1,18 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { container, textWrap, image, background } from './SpeakerCard.module.scss';
-import { useIntersection } from "use-intersection";
 
 interface SpeakerCardProps {
   speaker: Speaker;
 }
 
 const SpeakerCard: React.FC<SpeakerCardProps> = ({ speaker }) => {
-  const ref = useRef<HTMLDivElement>();
-  const isVisible = useIntersection(ref, { once: true });
   return (
-    <div ref={ref} className={container}>
+    <div className={container}>
       <div className={textWrap}>
         <span>{speaker.company}</span>
         <h3>{speaker.name}</h3>
       </div>
-      <img className={image} src={isVisible ? speaker.imageUrl : null} alt={speaker.name}/>
+      <img className={image} src={speaker.imageUrl} alt={speaker.name}/>
       <div className={background}/>
     </div>
   );

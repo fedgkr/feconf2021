f02f0d8ac49ddf601118750ec37e8aa7fa720eca
Donggeun lee <eastroots92@gmail.com>
2021-10-14 23:19:40 +0900

    Update sponsor urls

--- src/data/db/sponsors.ts
+++ src/data/db/sponsors.ts
@@ -2,13 +2,13 @@ export const sponsors: Sponsor[] = [
   {
     grade: 'Diamond',
     name: '비바리퍼블리카',
-    link: 'https://toss.im',
+    link: 'https://toss.im/career/jobs?search=Frontend%20Developer',
     imageUrl: '/images/sponsors/toss.png',
   },
   {
     grade: 'Platinum',
     name: '네이버',
-    link: 'https://www.navercorp.com/',
+    link: 'https://d2.naver.com/news/5211669',
     imageUrl: '/images/sponsors/naver.png',
   },
   {
@@ -20,31 +20,31 @@ export const sponsors: Sponsor[] = [
   {
     grade: 'Platinum',
     name: '당근마켓',
-    link: 'https://www.daangn.com/',
+    link: 'https://team.daangn.com/',
     imageUrl: '/images/sponsors/karrot.png',
   },
   {
     grade: 'Platinum',
     name: '와탭랩스',
-    link: 'https://www.whatap.io/ko/',
+    link: 'https://www.whatap.io/ko/careers/?utm_source=referral&utm_medium=banner&utm_campaign=feconf2021',
     imageUrl: '/images/sponsors/whatap.png',
   },
   {
     grade: 'Platinum',
     name: '벨루가브루어리',
-    link: 'https://business.veluga.kr/',
+    link: 'https://success.veluga.kr',
     imageUrl: '/images/sponsors/veluga.png',
   },
   {
     grade: 'Platinum',
     name: '버킷플레이스',
-    link: 'https://www.bucketplace.co.kr/',
+    link: 'https://www.bucketplace.co.kr/recruit/?team=dev',
     imageUrl: '/images/sponsors/ohouse.png',
   },
   {
     grade: 'Platinum',
     name: '레몬베이스',
-    link: 'https://www.lemonbase.team/',
+    link: 'https://www.lemonbase.team/?utm_source=FEConf2021&utm_medium=referral&utm_campaign=sponsor&utm_content=sponsor&utm_term=sponsor',
     imageUrl: '/images/sponsors/lemonbase.png',
   },
   {
@@ -55,14 +55,14 @@ export const sponsors: Sponsor[] = [
   },
   {
     grade: 'Gold',
-    name: 'codesoom',
+    name: '코드숨',
     link: 'https://www.codesoom.com/',
     imageUrl: '/images/sponsors/codesoom.png',
   },
   {
     grade: 'Platinum',
     name: '매스프레소',
-    link: 'https://mathpresso.com/en',
+    link: 'https://bit.ly/3FATqZP',
     imageUrl: '/images/sponsors/mathpresso.png',
   },
   {

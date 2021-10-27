fee23cf4d6076b926a9b0df7afc6f26c15139e5b
codemilli <hckrmoon@gmail.com>
2021-10-27 22:12:52 +0900

    파비콘 사이즈 추가

--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -34,8 +34,10 @@ const Meta: React.FC<MetaProps> = () => {
         content="국내 최고의 프론트엔드 개발 컨퍼런스. 프론트엔드 개발의 소중한 경험을 공유합니다!"
       />
       <meta name="twitter:creator" content="@FeConf" />
-      <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
-      <link rel="icon" type="image/x-icon" sizes="64x64" href="/favicon.ico"/>
+      <link rel="shortcut icon" type="image/x-icon" href="/favicon.png"/>
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
       <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css" />
     </Helmet>
   );
